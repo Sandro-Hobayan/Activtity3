6d7f5cfb345b3d0078d41beb00239933082c5cb8
hobayan hobayan <alesandrosfboo@gmail.com>
2024-08-24 14:16:15 +0800

    add Data Types

--- Main.java
+++ Main.java
@@ -3,8 +3,16 @@ public class Main {
     System.out.println("Hello World");
 
 	int myNum = 15;
-myNum = 20;  // myNum is now 20
+myNum = 20;  // myNum is now 20  // variables
 System.out.println(myNum);
+
+
+	int myNum = 5;               
+float myFloatNum = 5.99f;    
+char myLetter = 'D';         //datatypes
+boolean myBool = true;       
+String myText = "Hello";     
+
 
   }
 }
