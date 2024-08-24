1b246796103f85b40e52406105b475a9298e4f78
hobayan hobayan <alesandrosfboo@gmail.com>
2024-08-24 14:26:48 +0800

    nag add ng operators sa castings

--- Casting.java
+++ Casting.java
@@ -5,5 +5,19 @@ public class Casting {
 
     System.out.println(myInt);      // Outputs 9
     System.out.println(myDouble);   // Outputs 9.0
+
+
+
+
+
+int sum1 = 100 + 50;        // 150 (100 + 50)
+int sum2 = sum1 + 250;      // 400 (150 + 250)     //nag add po ako operators
+int sum3 = sum2 + sum2;     // 800 (400 + 400)
+
+
+
+
+
+
   }
 }
